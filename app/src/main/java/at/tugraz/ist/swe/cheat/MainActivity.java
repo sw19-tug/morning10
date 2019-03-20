519195fe548fce06f1efa058dd9d3f46e61ceb22
Patrick Leiter <p.leiter@student.tugraz.at>
2019-03-20 14:35:06 +0100

    CHEAT_005_001 add send button and listeners [OP,LP]

--- app/src/main/java/at/tugraz/ist/swe/cheat/MainActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/cheat/MainActivity.java
@@ -2,6 +2,13 @@ package at.tugraz.ist.swe.cheat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize tfInput and btSend
+        final EditText tfInput = findViewById(R.id.tf_input);
+        final Button btSend = findViewById(R.id.bt_send);
+
+        // TextChangedListener tfInput
+        tfInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                btSend.setEnabled(s.toString().isEmpty() ? false : true);
+            }
+        });
+
+        // OnClickListener sendButton
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tfInput.setText("");
+            }
+        });
     }
+
 }
