aa9dc76ce8187b1ecd37901dd399906c4da01dcb
Peter Oberrauner <peter.oberrauner@student.tugraz.at>
2019-04-03 10:22:56 +0200

    CHEAT_006_001 add message tests [SA, OP]

---
+++ app/src/test/java/at/tugraz/ist/swe/cheat/ChatUnitTests.java
@@ -0,0 +1,61 @@
+package at.tugraz.ist.swe.cheat;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothProfile;
+import android.os.Parcelable;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ChatUnitTests {
+
+    private static final String DEVICE_ADDRESS = "00:d0a:95:9d:68:16";
+    private static final String DEVICE_NAME = "Falco's Blackberry";
+
+    @Mock BluetoothDevice sender;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        when(sender.getAddress()).thenReturn(DEVICE_ADDRESS);
+        when(sender.getName()).thenReturn(DEVICE_NAME);
+    }
+
+    @Test
+    public void messageConstructor() {
+        final String MESSAGE = "Hello Receiver!";
+        Message messageObj = new Message(sender, MESSAGE);
+        assertEquals(sender, messageObj.getSender());
+        assertEquals(MESSAGE, messageObj.getMessage());
+    }
+
+    @Test
+    public void messageConstructor() {
+        final String MESSAGE = "Hello Receiver!";
+        Message messageObj = new Message(sender, MESSAGE);
+        assertEquals(sender, messageObj.getSender());
+        assertEquals(MESSAGE, messageObj.getMessage());
+    }
+
+    @Test
+    public void messageImplementsParcelable() {
+        final String MESSAGE = "Hello Receiver!";
+        Message messageObj = new Message(sender, MESSAGE);
+        assertTrue(messageObj instanceof Parcelable);
+    }
+}
