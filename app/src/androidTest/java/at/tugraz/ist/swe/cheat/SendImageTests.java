3e6b4f66398cc6f0d89c7068a0c32b6e9a3e18fa
Peter Oberrauner <peter.oberrauner@student.tugraz.at>
2019-05-15 11:11:10 +0200

    CHEAT_012_001 stub pick image intent [OP, SM]

--- app/src/androidTest/java/at/tugraz/ist/swe/cheat/SendImageTests.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/cheat/SendImageTests.java
@@ -1,10 +1,14 @@
 package at.tugraz.ist.swe.cheat;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +18,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
@@ -32,6 +37,7 @@ public class SendImageTests {
     @Rule
     public IntentsTestRule<MainActivity> mainActivityTestRule = new IntentsTestRule<>(MainActivity.class);
 
+
     @Test  // Test if the button is displayed
     public void testButtonVisible() {
         onView(withId(R.id.bt_sendImage)).check(matches(isDisplayed()));
@@ -39,6 +45,10 @@ public class SendImageTests {
 
     @Test  // Test if gallery intend is shown
     public void testGalleryIntend() {
+        Intent resultData = new Intent();
+        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+        intending(hasAction(Intent.ACTION_PICK)).respondWith(result);
+
         onView(withId(R.id.bt_sendImage)).perform(click());
         intended(hasAction(Intent.ACTION_PICK));
     }
