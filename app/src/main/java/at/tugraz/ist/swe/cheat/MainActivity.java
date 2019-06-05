f17d6176911af9a89067ca6ee21a3526198ac2f5
Mathias Schmolli <m.schmolli@student.tugraz.at>
2019-06-05 10:45:50 +0200

    CHEAT_013_001 store images taken with camera [OP,SM]

--- app/src/main/java/at/tugraz/ist/swe/cheat/MainActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/cheat/MainActivity.java
@@ -13,12 +13,14 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -36,7 +38,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import java.io.Console;
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Observable;
@@ -75,6 +80,8 @@ public class MainActivity extends AppCompatActivity implements ChatHistoryAdapte
 
     private static final int LOCATION_PERMISSION_RESPONSE = 2;
     private static final int READ_PERMISSION_RESPONSE = 3;
+
+    Uri takenImage = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -234,9 +241,31 @@ public class MainActivity extends AppCompatActivity implements ChatHistoryAdapte
         btnSendCameraImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (i.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(i, RESULT_IMAGE_TAKEN);
+                File image = null;
+                Intent camera_image_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (camera_image_intent.resolveActivity(getPackageManager()) != null) {
+                    try {
+                        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                        String imageFileName = "JPEG_" + timeStamp + "_";
+                        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                        image = File.createTempFile(
+                                imageFileName,  /* prefix */
+                                ".jpg",         /* suffix */
+                                storageDir      /* directory */
+                        );
+                    } catch (IOException ex) {
+
+                    }
+
+                    if (image != null) {
+                        Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                                "com.example.android.fileprovider",
+                                image);
+                        camera_image_intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        takenImage = photoURI;
+                        startActivityForResult(camera_image_intent, RESULT_IMAGE_TAKEN);
+
+                    }
                 }
             }
         });
@@ -363,28 +392,34 @@ public class MainActivity extends AppCompatActivity implements ChatHistoryAdapte
     }
 
     public void onActivityResult(int requestCode,int resultCode,Intent data){
+        Uri selectedImage = null;
 
-        if (resultCode == Activity.RESULT_OK)
-            switch (requestCode){
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case RESULT_IMAGE_TAKEN:
+                    selectedImage = takenImage;
+                    break;
                 case RESULT_IMAGE_SELECTED:
-                    Uri selectedImage = data.getData();
-                    try {
-                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
-                        String address;
-                        if (messageColor) {
-                            messageColor = false;
-                            address = "00:00:00:00:00:00";
-                        }
-                        else {
-                            messageColor = true;
-                            address = "11:00:00:00:00:00";
-                        }
-                        adapter.addMessage(new ChatMessage(1, address, bitmap, new Date()));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    selectedImage = data.getData();
                     break;
             }
+            if(selectedImage != null) {
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                    String address;
+                    if (messageColor) {
+                        messageColor = false;
+                        address = "00:00:00:00:00:00";
+                    } else {
+                        messageColor = true;
+                        address = "11:00:00:00:00:00";
+                    }
+                    adapter.addMessage(new ChatMessage(1, address, bitmap, new Date()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 
