9e41ad3c5a557a751cdec16780fa942093b8395b
Alexander Kropiunig <alexander.kropiunig@student.tugraz.at>
2019-06-12 10:17:13 +0200

    CHEAT_018_001 added TextToSpeech Tests [KA, OP]

---
+++ app/src/androidTest/java/at/tugraz/ist/swe/cheat/TextToSpeechTest.java
@@ -0,0 +1,71 @@
+package at.tugraz.ist.swe.cheat;
+
+import android.os.SystemClock;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withResourceName;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+
+@RunWith(AndroidJUnit4.class)
+public class TextToSpeechTest {
+
+        @Rule
+        public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+
+        // Check if Read to me is visible
+        @Test
+        public void optionDisplayed() {
+            final String message = "Hello World";
+            onView(withId(R.id.tf_input)).perform(replaceText(message));
+            onView(withId(R.id.bt_send)).perform(click());
+
+            onView(allOf(withId(R.id.tv_message), withText(message))).perform(longClick());
+            onView(withText("Read to me")).check(matches(isDisplayed()));
+
+        }
+/*
+        // Check if editing a message works
+        @Test
+        public void testEditMessage () {
+            final String message = "Hello World";
+            final String new_message = "Hola El Mundo";
+
+            onView(withId(R.id.tf_input)).perform(replaceText(message));
+            onView(withId(R.id.bt_send)).check(matches(withText("Send")));
+            onView(withId(R.id.bt_send)).perform(click());
+
+            onView(allOf(withId(R.id.tv_message), withText(message))).perform(longClick());
+            onView(withText("Edit")).perform(click());
+
+            onView(withId(R.id.bt_send)).check(matches(withText("Edit")));
+            onView(withId(R.id.tf_input)).check(matches(withText(message)));
+            onView(withId(R.id.tf_input)).perform(replaceText(new_message));
+            onView(withId(R.id.bt_send)).perform(click());
+
+            onView(withId(R.id.bt_send)).check(matches(withText("Send")));
+            onView(allOf(withId(R.id.tv_message), withText(new_message))).check(matches(isDisplayed()));
+            onView(allOf(withId(R.id.tv_message), withText(message))).check(doesNotExist());
+        }
+*/
+
+}
